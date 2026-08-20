abdb393dd17031ca4d329394972bd97433152c5d
VinceHark <vh@vincehark.com>
2026-08-20 16:06:39 -0600

    build: add KeyboardShortcuts dependency

--- Package.swift
+++ Package.swift
@@ -4,9 +4,17 @@ import PackageDescription
 let package = Package(
     name: "MacTLM",
     platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "3.0.1"),
+    ],
     targets: [
         .target(name: "MacTLMCore"),
-        .executableTarget(name: "MacTLM", dependencies: ["MacTLMCore"]),
+        .executableTarget(
+            name: "MacTLM",
+            dependencies: [
+                "MacTLMCore",
+                .product(name: "KeyboardShortcuts", package: "KeyboardShortcuts"),
+            ]),
         .testTarget(name: "MacTLMCoreTests", dependencies: ["MacTLMCore"]),
     ]
 )
